bea606ab860c171994482998d447b632f588c816
lmeller-git <louis.meller@icloud.com>
2025-10-04 13:45:54 +0200

    remove device specifc fish configs

--- config.fish
+++ config.fish
@@ -3,14 +3,9 @@ if status is-interactive
 end
 
 # >>> PATH >>>
-set -gx PATH $PATH /home/louis/.local/bin
-set -gx PATH $PATH /home/linuxbrew/.linuxbrew/bin
-set -gx PATH $PATH /home/louis/software/qemu/build
-set -gx PATH $PATH /home/louis/Coding/term
 # <<< PATH <<<
 
 # >>> Software init >>>
-export HELIX_RUNTIME=~/software/helix/runtime
 fzf --fish | source
 zoxide init fish --cmd cd | source
 starship init fish | source
@@ -25,16 +20,3 @@ alias l='ls -CF'
 alias ls='eza'
 alias lsa='ls -lAh --color=auto'
 # <<< custom aliases <<<
-
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-if test -f /home/louis/miniconda3/bin/conda
-    eval /home/louis/miniconda3/bin/conda "shell.fish" hook $argv | source
-else
-    if test -f "/home/louis/miniconda3/etc/fish/conf.d/conda.fish"
-        . "/home/louis/miniconda3/etc/fish/conf.d/conda.fish"
-    else
-        set -x PATH /home/louis/miniconda3/bin $PATH
-    end
-end
-# <<< conda initialize <<<
